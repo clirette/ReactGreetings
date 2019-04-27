cf6b56a1fec71e0f7108a922051e0734f085f41b
Chase Lirette <clirette@dxc.com>
2019-04-26 20:49:40 -0500

    proxy

--- src/App.js
+++ src/App.js
@@ -1,32 +1,44 @@
-import React, { useState } from 'react';
-import axios from 'axios';
-import './App.css';
+import React, { useState } from "react";
+import axios from "axios";
+import "./App.css";
 
-function GreetingForm({createGreeting}) {
-  const [name, setName] = useState('');
+const PROD =
+  "https://cors-anywhere.herokuapp.com/http://ec2-34-227-13-253.compute-1.amazonaws.com:8080";
+
+function GreetingForm({ createGreeting }) {
+  const [name, setName] = useState("");
 
   const handleSubmit = async e => {
     e.preventDefault();
     try {
-      const response = await axios.get(`/greeting${name ? `/?name=${name}` : ''}`);
+      const response = await axios.get(
+        `${PROD}/greeting${name ? `/?name=${name}` : ""}`
+      );
       console.log(response.data);
       createGreeting(response.data);
-    } catch(err) {
+    } catch (err) {
       console.log(err);
     }
-  }
+  };
 
   return (
     <div>
       <form onSubmit={handleSubmit}>
-        <input type="text" name="name" className="greeting-form__input" value={name} placeholder="Enter your name" onChange={e => setName(e.target.value)}/>
-        <input type="submit" className="greeting-form__submit" value="Submit"/>
+        <input
+          type="text"
+          name="name"
+          className="greeting-form__input"
+          value={name}
+          placeholder="Enter your name"
+          onChange={e => setName(e.target.value)}
+        />
+        <input type="submit" className="greeting-form__submit" value="Submit" />
       </form>
     </div>
-  )
+  );
 }
 
-function Greeting({greeting}) {
+function Greeting({ greeting }) {
   return (
     <div>
       <div className="greeting-block">
@@ -42,15 +54,15 @@ function Greeting({greeting}) {
         <p className="greeting-block__content">{JSON.stringify(greeting)}</p>
       </div>
     </div>
-  )
+  );
 }
 
 function App() {
-  const [greeting, setGreeting] = useState('');
+  const [greeting, setGreeting] = useState("");
 
   const createGreeting = createdGreeting => {
     setGreeting(createdGreeting);
-  }
+  };
 
   return (
     <div className="App">
@@ -58,13 +70,13 @@ function App() {
         <div className="heading-text-box">
           <h1 className="heading-text-box__title">Greetings!</h1>
           <div className="greeting-form">
-            <GreetingForm createGreeting={createGreeting}/>
+            <GreetingForm createGreeting={createGreeting} />
           </div>
         </div>
       </div>
       <div className="greeting">
         <div className="greeting__text">
-          {greeting && <Greeting greeting={greeting}/>}
+          {greeting && <Greeting greeting={greeting} />}
         </div>
       </div>
     </div>
